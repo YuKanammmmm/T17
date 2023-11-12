89f65a2330b359469d6b21cf2f6210746ac4f1d6
Suntend <2822811689@qq.com>
2023-11-12 20:42:21 +0000

    拿到凸包的点

--- project.py
+++ project.py
@@ -1,30 +1,21 @@
+import logging
 import numpy as np
 import cv2
+
 font = cv2.FONT_HERSHEY_SIMPLEX
 lower_yellow = np.array([20, 100, 100])  
 upper_yellow = np.array([40, 255, 255])  
 lower_blue = np.array([110, 100, 100])  
 upper_blue = np.array([130, 255, 255])
-rho = 1
-theta = np.pi/180
-threshold = 500
 
-def draw_lines(img, houghLines, color=[255, 0, 0], thickness=2):
-    for line in houghLines:
-        for rho,theta in line:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*rho
-            y0 = b*rho
-            x1 = int(x0 + 1000*(-b))
-            y1 = int(y0 + 1000*(a))
-            x2 = int(x0 - 1000*(-b))
-            y2 = int(y0 - 1000*(a))
+# 视频的分辨率
+pixiv_x = 1920
+pixiv_y = 1088
 
-            cv2.line(img,(x1,y1),(x2,y2),color,thickness)
+key_points = []
+key_points2 = []
 
-cap = cv2.VideoCapture("E:/anaconda3/test2.mp4")  
-#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("C:/Users/28228/Desktop/Subject1.mp4")
 if (cap.isOpened()):  
     flag = 1
 else:
@@ -32,7 +23,7 @@ else:
 num = 0
 if (flag):
     while (True):
-        ret, frame = cap.read() 
+        ret, frame = cap.read()
        
         if ret == False:  
             break
@@ -51,22 +42,50 @@ if (flag):
         cv2.polylines(frame, [hull], True, (0, 255, 0), 2)
         cv2.polylines(frame, [hull2], True, (0, 255, 0), 2)
 
-        #
-        ## white and black
-        img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # gray_y = cv2.cvtColor(mask_yellow, cv2.COLOR_BGR2GRAY)
+        # edge_y = cv2.Canny(gray_y, 50, 50)
+        # line_y = cv2.HoughLines(edge_y, rho, theta, threshold)
+        # draw_hough_lines(frame, line_y)
+
+        # for point in contours[0]:
+        for point in hull:
+            p = point[0]
+            x = p[0].astype(np.int)
+            y = p[1].astype(np.int)
+            key_points.append((x,y))
+            cv2.circle(frame, (x, y), 2, [255,0,0], 2)
+            
+        for point in hull2:
+            p = point[0]
+            x = p[0].astype(np.int)
+            y = p[1].astype(np.int)
+            key_points2.append((x,y))
+            cv2.circle(frame, (x, y), 2, [255,0,0], 2)
         
-        ## glass
-        img_blur = cv2.GaussianBlur(img_gray, (5,5), 0)
+        # for key_point in key_points:
+        #     conver_point_temp = []
+        #     for theta in range(-90, 90):
+        #         x, y = key_point
+        #         rad = theta / 180 * np.pi
+        #         rho = x * np.cos(rad) + y * np.sin(rad)
+        #         conver_point_temp.append((int(theta), int(rho)))
+        #     conver_points.append(conver_point_temp)
+
+        # for conver_point in conver_points:
+        #     for point in conver_point:
+        #         theta, rho = point
+        #         key = f"{theta}{rho}"
+        #         if key not in point_vote:
+        #             point_vote[key] = 0
+        #         point_vote[key] += 1
         
-        ## edge
-        img_edges = cv2.Canny(img_blur, 50, 120)
-        
-        hough = cv2.HoughLines(img_edges, rho, theta, threshold)
-        hough2 = cv2.HoughLines(img_edges, rho, theta, threshold)
-        draw_lines(np.zeros_like(frame), hough)
-        draw_lines( np.zeros_like(frame), hough2)
-        #
-        
+        # result = sorted(point_vote.items(), key=lambda d: d[1], reverse=True)[0][0]
+        # result = tuple(map(int, result.split()))
+        # theta, rho = result
+        # radd = theta / 180 * np.pi
+        # getY = lambda r, t, x: int((rho-x*np.cos(radd)))/np.sin(radd)
+        # cv2.line(frame, (0, getY(rho,theta,0)), (pixiv_x, getY(rho,theta, pixiv_y)), 255)
+
         num = num + 1
         cv2.imshow("result", frame)
         cv2.imwrite("imgs/%d.jpg"%num, frame)
